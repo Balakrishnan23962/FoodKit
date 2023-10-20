ba758aeb687565ac24ba2ac98aa9c375733326e9
krish962 <balakrishnan.logicfocus@gmail.com>
2023-10-20 10:50:59 +0530

    Initial Commit

---
+++ FoodKit/FoodKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodKitApp.swift
+//  FoodKit
+//
+//  Created by Logicfocus Techonology Solution on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
